c5b8eab451304c02d7e882fa7f1b1e0655fa4919
Lev Bazylskyi <chozen.lev@gmail.com>
2018-03-20 22:13:21 +0200

    added initial input

--- lab1/lab1.py
+++ lab1/lab1.py
@@ -1,6 +1,9 @@
 from xml.dom.minidom import parse
 from datetime import datetime
 
+program_type_all = 'Всі'
+program_type_default = 'Серіали'
+
 def get_timetable(path_html):
     htmldoc = parse(path_html)
     timetable = []
@@ -20,22 +23,16 @@ def get_files(path_xml):
     xmldoc.unlink()
     return files
 
-def printProgram(program):
-    print(program[0] + ' - ' + program[1] + ' (' + program[2] + ')')
-
 def printChannel(channel):
     print(channel[0])
     for program in channel[1]:
-        print('   ', end = '')
-        printProgram(program)
+        print('   ' + program[0] + ' - ' + program[1] + ' (' + program[2] + ')')
 
-def filterDate(timetable, start, end):
+def filterDate(timetable, datetime_start, datetime_end):
     filtered = []
-    datetime_start = datetime.strptime(start, '%d.%m.%Y %H:%M')
-    datetime_end = datetime.strptime(end, '%d.%m.%Y %H:%M')
     for program in timetable:
         datetime_current = datetime.strptime(program[0], '%d.%m.%Y %H:%M')
-        if ((datetime_current >= datetime_start) and (datetime_current <= datetime_end)):
+        if (datetime_current >= datetime_start and datetime_current <= datetime_end):
             filtered.append(program)
 
     return filtered
@@ -50,16 +47,94 @@ def filterType(timetable, type):
 
 def filter(timetable, date_start, date_end, type_str):
     filtered = filterDate(timetable, date_start, date_end)
-    filtered = filterType(filtered, type_str)
+    if type_str and type_str != program_type_all:
+        filtered = filterType(filtered, type_str)
     return filtered
+
+def getAllTypes(timetables):
+    types = set()
+    for timetable in timetables:
+        for program in timetable[1]:
+            types.add(program[2])
+
+    return types
+
+def getMinDate(timetables):
+    datetime_min = None;
+    for timetable in timetables:
+        for program in timetable[1]:
+            datetime_current = datetime.strptime(program[0], '%d.%m.%Y %H:%M')
+            if not datetime_min or datetime_current < datetime_min:
+                datetime_min = datetime_current
+    return datetime_min.strftime('%d.%m.%Y %H:%M')
+
+
+def getMaxDate(timetables):
+    datetime_max = None;
+    for timetable in timetables:
+        for program in timetable[1]:
+            datetime_current = datetime.strptime(program[0], '%d.%m.%Y %H:%M')
+            if not datetime_max or datetime_current > datetime_max:
+                datetime_max = datetime_current
+    return datetime_max.strftime('%d.%m.%Y %H:%M')
 
 if __name__ == '__main__':
     channels = get_files('channels.xml')
     timetables = [(channel[1], get_timetable(channel[0])) for channel in channels]
-    filtered_timetable = [[timetable[0], filter(timetable[1], '19.03.2018 06:29', '22.03.2018 10:29', 'Серіали')] for timetable in timetables]
+    datetime_min = getMinDate(timetables)
+    datetime_max = getMaxDate(timetables)
 
-    for i, channel in enumerate(filtered_timetable):
+    print('Типи передач: ' + ', '.join(str(type) for type in getAllTypes(timetables)))
+
+    while True:
+        print('Введіть першу дату[' + str(datetime_min) + ']: ', end = '')
+        datetime_start = input()
+        if not datetime_start:
+            datetime_start = datetime_min
+
+        try:
+            datetime_start = datetime.strptime(datetime_start, '%d.%m.%Y %H:%M')
+        except ValueError:
+            print('Невірно введена дата, спробуйте ще раз')
+        except:
+            exit()
+        else:
+            if not datetime_start:
+                datetime_start = datetime_min
+            break
+
+    while True:
+        print('Введіть другу дату[' + str(datetime_max) + ']: ', end = '')
+        datetime_end = input()
+        if not datetime_end:
+            datetime_end = datetime_max
+            
+        try:
+            datetime_end = datetime.strptime(datetime_end, '%d.%m.%Y %H:%M')
+        except ValueError:
+            print('Невірно введена дата, спробуйте ще раз')
+        except:
+            exit()
+        else:
+            if not datetime_end:
+                datetime_end = datetime_max
+            break
+
+    print('Введіть тип телепрограми[' + program_type_default + ']: ',  end = '')
+    program_type = input()
+    if not program_type:
+        program_type = program_type_default
+
+    filtered_timetable = [(timetable[0], filter(timetable[1], datetime_start, datetime_end, program_type)) for timetable in timetables]
+
+    print('\nРезультат пошуку:')
+    i = 0
+    for channel in filtered_timetable:
         if channel[1]:
-            print(str(i + 1) + '. ', end = '')
+            i += 1
+            print(str(i) + '. ', end = '')
             printChannel(channel)
             print()
+    
+    if i == 0:
+        print('    ** None **')
