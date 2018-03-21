07eec20aabee7601665163e5b109d911e79cd9d9
Lev Bazylskyi <chozen.lev@gmail.com>
2018-03-21 02:30:01 +0200

    removed virtualenv and added command line parameters

--- lab1/lab1.py
+++ lab1/lab1.py
@@ -1,41 +1,48 @@
 from xml.dom.minidom import parse
 from datetime import datetime
+import sys
 
 program_type_all = 'Всі'
 program_type_default = 'Серіали'
+
+
+def get_files(path_xml):
+    xmldoc = parse(path_xml)
+    items = xmldoc.getElementsByTagName('item')
+    files = [(t.childNodes[0].data, t.attributes['name'].value) for t in items]
+    xmldoc.unlink()
+    return files
+
 
 def get_timetable(path_html):
     htmldoc = parse(path_html)
     timetable = []
 
     for tr in htmldoc.getElementsByTagName('tr'):
-        program = [td.childNodes[0].data for td in tr.getElementsByTagName('td')]
+        tds = tr.getElementsByTagName('td')
+        program = [td.childNodes[0].data for td in tds]
         if (program):
             timetable.append(program)
-    
+
     htmldoc.unlink()
     return timetable
 
-def get_files(path_xml):
-    xmldoc = parse(path_xml)
-    items = xmldoc.getElementsByTagName('item')
-    files = [(item.childNodes[0].data, item.attributes['name'].value) for item in items]
-    xmldoc.unlink()
-    return files
 
 def printChannel(channel):
     print(channel[0])
-    for program in channel[1]:
-        print('   ' + program[0] + ' - ' + program[1] + ' (' + program[2] + ')')
+    for prog in channel[1]:
+        print('   ' + prog[0] + ' - ' + prog[1] + ' (' + prog[2] + ')')
 
-def filterDate(timetable, datetime_start, datetime_end):
+
+def filterDate(timetable, start, end):
     filtered = []
     for program in timetable:
-        datetime_current = datetime.strptime(program[0], '%d.%m.%Y %H:%M')
-        if (datetime_current >= datetime_start and datetime_current <= datetime_end):
+        current = datetime.strptime(program[0], '%d.%m.%Y %H:%M')
+        if (current >= start and current <= end):
             filtered.append(program)
 
     return filtered
+
 
 def filterType(timetable, type):
     filtered = []
@@ -45,96 +52,55 @@ def filterType(timetable, type):
 
     return filtered
 
+
 def filter(timetable, date_start, date_end, type_str):
     filtered = filterDate(timetable, date_start, date_end)
     if type_str and type_str != program_type_all:
         filtered = filterType(filtered, type_str)
+
     return filtered
 
-def getAllTypes(timetables):
-    types = set()
-    for timetable in timetables:
-        for program in timetable[1]:
-            types.add(program[2])
-
-    return types
-
-def getMinDate(timetables):
-    datetime_min = None;
-    for timetable in timetables:
-        for program in timetable[1]:
-            datetime_current = datetime.strptime(program[0], '%d.%m.%Y %H:%M')
-            if not datetime_min or datetime_current < datetime_min:
-                datetime_min = datetime_current
-    return datetime_min.strftime('%d.%m.%Y %H:%M')
-
-
-def getMaxDate(timetables):
-    datetime_max = None;
-    for timetable in timetables:
-        for program in timetable[1]:
-            datetime_current = datetime.strptime(program[0], '%d.%m.%Y %H:%M')
-            if not datetime_max or datetime_current > datetime_max:
-                datetime_max = datetime_current
-    return datetime_max.strftime('%d.%m.%Y %H:%M')
 
 if __name__ == '__main__':
     channels = get_files('channels.xml')
-    timetables = [(channel[1], get_timetable(channel[0])) for channel in channels]
-    datetime_min = getMinDate(timetables)
-    datetime_max = getMaxDate(timetables)
+    timetables = [(ch[1], get_timetable(ch[0])) for ch in channels]
 
-    print('Типи передач: ' + ', '.join(str(type) for type in getAllTypes(timetables)))
+    datetime_start = sys.argv[1] if len(sys.argv) >= 2 else None
+    datetime_end = sys.argv[2] if len(sys.argv) >= 3 else None
+    program_type = sys.argv[3] if len(sys.argv) >= 4 else None
 
-    while True:
-        print('Введіть першу дату[' + str(datetime_min) + ']: ', end = '')
-        datetime_start = input()
-        if not datetime_start:
-            datetime_start = datetime_min
-
+    if datetime_start:
         try:
-            datetime_start = datetime.strptime(datetime_start, '%d.%m.%Y %H:%M')
+            datetime_start = datetime.strptime(datetime_start,
+                                               '%d.%m.%Y %H:%M')
         except ValueError:
-            print('Невірно введена дата, спробуйте ще раз')
-        except:
-            exit()
-        else:
-            if not datetime_start:
-                datetime_start = datetime_min
-            break
+            sys.exit('Невірно введена перша дата')
 
-    while True:
-        print('Введіть другу дату[' + str(datetime_max) + ']: ', end = '')
-        datetime_end = input()
-        if not datetime_end:
-            datetime_end = datetime_max
-            
+    if datetime_end:
         try:
             datetime_end = datetime.strptime(datetime_end, '%d.%m.%Y %H:%M')
         except ValueError:
-            print('Невірно введена дата, спробуйте ще раз')
-        except:
-            exit()
-        else:
-            if not datetime_end:
-                datetime_end = datetime_max
-            break
+            sys.exit('Невірно введена друга дата')
 
-    print('Введіть тип телепрограми[' + program_type_default + ']: ',  end = '')
-    program_type = input()
     if not program_type:
         program_type = program_type_default
 
-    filtered_timetable = [(timetable[0], filter(timetable[1], datetime_start, datetime_end, program_type)) for timetable in timetables]
+    filtered_timetables = []
+    for timetable in timetables:
+        filtered_channel = filter(timetable[1],
+                                  datetime_start,
+                                  datetime_end,
+                                  program_type)
+        filtered_timetables.append((timetable[0], filtered_channel))
 
-    print('\nРезультат пошуку:')
     i = 0
-    for channel in filtered_timetable:
+    print('Результат пошуку:')
+    for channel in filtered_timetables:
         if channel[1]:
             i += 1
-            print(str(i) + '. ', end = '')
+            print(str(i) + '. ', end='')
             printChannel(channel)
             print()
-    
+
     if i == 0:
         print('    ** None **')
